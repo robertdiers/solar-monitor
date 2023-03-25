2ef632e0c80d6d44e50be309f9d59c68ea33ab7b
Robert Diers <robertdiers@gmail.com>
2023-03-25 16:47:54 +0100

    removed docker image

--- image.sh
+++ image.sh
@@ -1,1 +1,1 @@
-sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t solarmonitor --push .
+sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t solarmonitor .
