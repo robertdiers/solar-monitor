ca3572f756a07c53a8105871528d2259c59542d9
Robert Diers <robert.diers@accenture.com>
2023-06-22 14:48:01 +0200

    podman multi-arch build

--- image.sh
+++ image.sh
@@ -1,1 +1,7 @@
-sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t solarmonitor .
+#sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t solarmonitor .
+
+sudo apt install -y podman buildah qemu-user-static
+podman build --log-level debug --platform linux/arm64/v8 --platform linux/amd64 --manifest ghcr.io/robertdiers/solar-monitor:0.1 .
+podman manifest inspect ghcr.io/robertdiers/solar-monitor:0.1
+podman images
+podman manifest push ghcr.io/robertdiers/solar-monitor:0.1 ghcr.io/robertdiers/solar-monitor:0.1
