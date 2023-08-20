c2d4c62e730a01bbf627d1dd0161aca85fa260b2
Robert Diers <robertdiers@gmail.com>
2023-08-20 19:23:00 +0200

    fix python install

--- pip_install.sh
+++ pip_install.sh
@@ -1,1 +1,2 @@
-pip3 install configparser pymodbus psycopg2-binary pyserial_asyncio pyserial requests
+#pip3 install configparser pymodbus psycopg2-binary pyserial_asyncio pyserial requests
+sudo apt install python3-pymodbus python3-psycopg2 python3-paho-mqtt python3-requests
