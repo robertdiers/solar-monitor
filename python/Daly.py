3623f3d6bf80a95f126c1f928ea4aeaf64fad07d
Robert Diers <robertdiers@gmail.com>
2024-08-11 10:55:43 +0200

    daly fix

--- python/Daly.py
+++ python/Daly.py
@@ -46,7 +46,7 @@ def subscribe(mqtt_broker, mqtt_port, mqtt_user, mqtt_password, topic, callback,
         internalcallback = callback
 
         # Set Connecting Client ID
-        client = mqtt_client.Client(client_id, userdata=client_userdata)
+        client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1, client_id, userdata=client_userdata)
         client.user_data_set(client_userdata)
         client.username_pw_set(mqtt_user, mqtt_password)
 
